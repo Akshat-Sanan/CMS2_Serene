3c781c81fee2221ac6d62c625506ff30194263fe
Akshat Sanan <sananakshat297@gmail.com>
2024-02-15 10:20:01 -0500

    added some extra plugins

--- plugins/serene-customPlugin/serene-customPlugin.php
+++ plugins/serene-customPlugin/serene-customPlugin.php
@@ -11,4 +11,12 @@
         filemtime( plugin_dir_path( __FILE__ ) . '/plugin.js' )
     );
 }
+
+wp_enqueue_style(
+    'serene-customPlugin-css',
+    plugins_url( 'style.css', __FILE__ ),
+    array(), // No dependencies
+    filemtime( plugin_dir_path( __FILE__ ) . '/style.css' ) // Version parameter to bust cache
+);
+
 add_action( 'enqueue_block_editor_assets', 'serene_enqueue_block_editor_assets' );
